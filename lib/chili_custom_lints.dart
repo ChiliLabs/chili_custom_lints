2ff8445222bd932477b100e678ae5c589b717d9d
darjaorlova <darja.orlova@chi.lv>
2023-07-24 10:44:19 +0300

    Create linter entry point

--- lib/chili_custom_lints.dart
+++ lib/chili_custom_lints.dart
@@ -1,3 +1,9 @@
-int calculate() {
-  return 6 * 7;
+// This is the entrypoint of our custom linter
+import 'package:custom_lint_builder/custom_lint_builder.dart';
+
+PluginBase createPlugin() => _ChiliCustomLinter();
+
+class _ChiliCustomLinter extends PluginBase {
+  @override
+  List<LintRule> getLintRules(CustomLintConfigs configs) => [];
 }
