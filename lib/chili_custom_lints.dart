a9db6766bd4776c1f28ed98b8e3a2cafed093a4c
darjaorlova <darja.orlova@chi.lv>
2023-07-24 20:41:22 +0300

    Add omit_explicit_parameters_types rule & fix

--- lib/chili_custom_lints.dart
+++ lib/chili_custom_lints.dart
@@ -1,12 +1,11 @@
 // This is the entrypoint of our custom linter
-import 'package:chili_custom_lints/lints/prefer_arrow_syntax/prefer_arrow_syntax_rule.dart';
+import 'package:chili_custom_lints/lints/omit_explicit_parameters_types/omit_explicit_parameters_types_rule.dart';
 import 'package:custom_lint_builder/custom_lint_builder.dart';
 
 PluginBase createPlugin() => _ChiliCustomLinter();
 
 class _ChiliCustomLinter extends PluginBase {
   @override
-  List<LintRule> getLintRules(CustomLintConfigs configs) => [
-    PreferArrowSyntaxRule()
-  ];
+  List<LintRule> getLintRules(CustomLintConfigs configs) =>
+      [OmitFunctionParamsTypesRule()];
 }
---
+++ lib/lints/omit_explicit_parameters_types/omit_explicit_parameters_types_rule.dart
@@ -0,0 +1,84 @@
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/error/error.dart';
+import 'package:analyzer/error/listener.dart';
+import 'package:analyzer/source/source_range.dart';
+import 'package:custom_lint_builder/custom_lint_builder.dart';
+
+class OmitFunctionParamsTypesRule extends DartLintRule {
+  OmitFunctionParamsTypesRule() : super(code: _code);
+
+  static final _code = LintCode(
+    name: 'omit_explicit_parameters_types',
+    problemMessage: 'Omit explicit parameters types',
+    correctionMessage:
+        'Omit explicit parameters types, e.g. from `void foo(int a, String b) { }` to `void foo(a, b) { }`',
+  );
+
+  @override
+  void run(
+    CustomLintResolver resolver,
+    ErrorReporter reporter,
+    CustomLintContext context,
+  ) {
+    context.registry.addNamedExpression((node) {
+      final expression = node.expression;
+      if (expression is FunctionExpression) {
+        final params = expression.parameters?.parameters ?? [];
+        for (var param in params) {
+          if (param is SimpleFormalParameter) {
+            if (param.type != null) {
+              reporter.reportErrorForNode(_code, param);
+            }
+          }
+        }
+      }
+    });
+  }
+
+  @override
+  List<Fix> getFixes() => [_OmitFunctionParamsTypesFix()];
+}
+
+class _OmitFunctionParamsTypesFix extends DartFix {
+  @override
+  void run(
+    CustomLintResolver resolver,
+    ChangeReporter reporter,
+    CustomLintContext context,
+    AnalysisError analysisError,
+    List<AnalysisError> others,
+  ) {
+    context.registry.addNamedExpression((node) {
+      if (!analysisError.sourceRange.intersects(node.sourceRange)) {
+        return;
+      }
+
+      final expression = node.expression;
+      if (expression is! FunctionExpression) {
+        return;
+      }
+
+      final params = expression.parameters?.parameters ?? [];
+      for (var param in params) {
+        if (param is SimpleFormalParameter) {
+          final type = param.type;
+          if (type != null) {
+            final changeBuilder = reporter.createChangeBuilder(
+              message: 'Omit explicit parameters types',
+              priority: 1,
+            );
+            changeBuilder.addDartFileEdit((builder) {
+              builder.addSimpleReplacement(
+                SourceRange(
+                  type.offset,
+                  type.length + 1,
+                ),
+                '',
+              );
+            });
+          }
+        }
+      }
+    });
+  }
+}
